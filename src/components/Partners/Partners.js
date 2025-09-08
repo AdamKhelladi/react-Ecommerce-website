be62d67b3a2c0f5bf5e14504ef8a8b03eeba5bc9
AdamKhelladi <khelladiadem5@gmail.com>
2025-09-08 11:34:42 +0100

    Dublicate the images to make a loop bar

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,4 +1,5 @@
 import "./Partners.css";
+
 import image1 from "../../assets/brand/br-1.png";
 import image2 from "../../assets/brand/br-2.png";
 import image3 from "../../assets/brand/br-3.png";
@@ -6,13 +7,19 @@ import image4 from "../../assets/brand/br-4.png";
 import image5 from "../../assets/brand/br-5.png";
 
 export default function Partners() {
+  const images = [image1, image2, image3, image4, image5];
+
   return (
-    <div className="partners-container">
-      <img src={image1} alt="Partner brand" />
-      <img src={image2} alt="Partner brand" />
-      <img src={image3} alt="Partner brand" />
-      <img src={image4} alt="Partner brand" />
-      <img src={image5} alt="Partner brand" />
+    <div className="partners-wrapper">
+      <div className="partners-container">
+        {images.map((img, index) => (
+          <img key={`first-${index}`} src={img} alt="Partner brand" />
+        ))}
+
+        {images.map((img, index) => (
+          <img key={`second-${index}`} src={img} alt="Partner brand" />
+        ))}
+      </div>
     </div>
   );
 }
